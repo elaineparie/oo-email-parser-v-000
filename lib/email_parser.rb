8f89b128c3a064219293e2913b05243a85525b02
Elaine Anderson <eparie@bu.edu>
2018-06-02 12:02:50 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,7 @@ def self.parse(csv_emails)
   email_list = []
   email_list = csv_emails.split(',')
   normalized_emails = self.new
-  normalized_emails.emails = email list
+  normalized_emails.emails = email_list
   self.parse(csv_emails)
 end
 
